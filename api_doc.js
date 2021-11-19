369aa4632fc862d4ca67989ab2dacb6377b3a96f
Tomas Figura <tomas.figura@post.cz>
2021-11-19 21:01:38 +0100

    Api documentation in api_doc.js

---
+++ api_doc.js
@@ -0,0 +1,23 @@
+/* Register user [POST] https://www.polkadot-hub.eu/api/register
+
+{
+	"username": "test",
+	"password": "test",
+	"age": 1255555555555555555555555555555555555555555555
+}
+
+Error Response Codes: 400 - Wrong request/Database error
+
+*/
+
+
+/* Login user [POST] https://www.polkadot-hub.eu/api/login
+
+{
+	"username": "test",
+	"password": "test"
+}
+
+Error Response Codes: 400 - Wrong request/Database error, 404 - User login was wrong
+
+*/
