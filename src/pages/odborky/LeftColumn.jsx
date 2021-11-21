c769a5bfb1427801cf5d4388a89fed719dcdd440
Filip Masný <filip.masny@gmail.com>
2021-11-21 19:24:17 +0100

    Update LeftColumn.jsx
    presunutie MyCard a Section componentov do osobitnych filov

--- src/pages/odborky/LeftColumn.jsx
+++ src/pages/odborky/LeftColumn.jsx
@@ -1,82 +1,18 @@
-import { fakeOdborky, fakeOdborky1 } from "../../utils/fakeData";
-import { Badge, Button, Card, Col } from "react-bootstrap";
-import React, { useState } from "react";
+import React from "react";
+import { Col } from "react-bootstrap";
+import Section from "../../components/preskumaj/Section";
 
-const LeftColumn = ({ setShowCardModal }) => {
-  const [hoveredCard, setHoveredCard] = useState(undefined);
-
-  const handleMouseOver = (id) => {
-    // setHoveredCard(id);
-  };
-
-  const handleMouseOut = () => {
-    setHoveredCard(undefined);
-  };
-
-  const MyCard = ({ id }) => {
+const LeftColumn = ({ setShowCardModal, vekKat, progKat }) => {
+  const sectionsMaker = vekKat.map((section) => {
     return (
-      <div
-        className="my-card"
-        style={{
-          display: "inline-block",
-          margin: "1%",
-          cursor: "pointer",
-          backgroundColor: "rgba(255, 255, 255, 0.5)",
-          zIndex: "99",
-        }}
-        onClick={() => setShowCardModal({})}
-        onMouseOver={() => handleMouseOver(id)}
-        onMouseOut={handleMouseOut}
-      >
-        <Card
-          style={{
-            padding: "1rem",
-            border: "none",
-            backgroundColor: "rgba(255, 255, 255, 0)",
-          }}
-          className="my-card"
-        >
-          {id % 3 === 1 && (
-            <Badge bg="warning" className="my-badge">
-              Bud prvy
-            </Badge>
-          )}
-          <Card.Img
-            style={{
-              padding: "1rem",
-              width: "10rem",
-              marginLeft: "auto",
-              marginRight: "auto",
-            }}
-            variant="top"
-            src="https://www.skauting.sk/wp-content/uploads/2017/04/skauting-program-odborka-vlcata-43-200x180.png"
-          />
-          <Card.Body style={{ backgroundColor: "rgba(255, 255, 255, 0)" }}>
-            <Card.Title className="text-center">Animáčik</Card.Title>
-            {hoveredCard === id && <Button>Hi</Button>}
-          </Card.Body>
-        </Card>
-      </div>
+      <Section
+        id={section.id}
+        name={section.name}
+        setShowCardModal={setShowCardModal}
+        progKat={progKat}
+      />
     );
-  };
-
-  const Section = ({ id }) => {
-    return (
-      <div id={id} style={{ marginBottom: "2.5rem" }}>
-        <h3>Vlčiacke a včielkarske odborky</h3>
-        <p style={{ width: "90%" }}>
-          U vĺčat a včielok majú mnohé odborky rozprávkový charakter (Žabí
-          princ, Hobit) alebo sa už zameriavajú na konkrétne reálne oblasti, na
-          ktoré neskôr nadväzujú aj skautské odborky (Táborník, Hvezdár).
-          Odborky často plnia kolektívne so skupinou priateľov. Majú iba jeden
-          stupeň a ich nášivka má trojuholníkový tvar a červený lem.
-        </p>
-        {fakeOdborky.map((_, id) => (
-          <MyCard id={id} />
-        ))}
-      </div>
-    );
-  };
+  });
 
   return (
     <Col
@@ -90,9 +26,7 @@ const LeftColumn = ({ setShowCardModal }) => {
         flexWrap: "wrap",
       }}
     >
-      <Section id="1" />
-      <Section id="2" />
-      <Section id="3" />
+      {sectionsMaker}
     </Col>
   );
 };
